540d1cf39400345127be692de1a51a81757a4ebf
linhtiendev <lelinhtien0707@gmail.com>
2024-08-19 09:41:37 +0700

    base

---
+++ frontend/src/components/router/Routers.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+const Routers = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Register />} />
+        </Routes>
+    );
+};
+
+export default Routers;
